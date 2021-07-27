e07a57234ce9ab6f4931419c1011f585c5367069
sath1 <baskalpa@yahoo.com>
2021-07-27 17:22:32 -0400

    i have done this

--- TestProject/src/AddOneToTen.java
+++ TestProject/src/AddOneToTen.java
@@ -2,6 +2,8 @@
 public class AddOneToTen {
 
 	public static void main(String[] args) {
+		
+		
 	for(int i=0;i<=10;i++)
 	{
 		System.out.println(i);
---
+++ TestProject/src/TrverseString.java
@@ -0,0 +1,23 @@
+
+public class TrverseString {
+
+	public static void main(String[] args) {
+	/*	String s = "ABCDEFGH";
+		String t =" ";
+		
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			System.out.println(t=t+s.charAt(i));
+		}*/
+		
+		
+		String s="WELCOME";
+		
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			System.out.println(s.charAt(i));
+		}
+
+	}
+
+}
